df384d8832e3ff331f969922109b225e6d299a39
Yurii Rybachok <drummer2009@ukr.net>
2023-11-04 13:37:05 +0200

    create header component & add basics styles for project

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,101 @@
 <template>
-  <div class="dashboard">Test</div>
+  <div class="dashboard">
+    <TheHeader />
+  </div>
 </template>
 
 <script>
+import TheHeader from '@/components/TheHeader.vue';
 export default {
   name: 'App',
-  components: {},
+  components: {
+    TheHeader
+  },
 };
 </script>
 
-<style></style>
+<style>
+:root {
+  --primary: #8A4CFC;
+  --primary-alt: #702fe7;
+  --light: #EEEEEE;
+  --light-alt: #F8F8F8;
+  --grey: #888888;
+  --dark: #131A26; 
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira sans", sans-serif;
+}
+
+body {
+  background-color: var(--light);
+}
+
+main {
+  width: 100vw;
+  overflow: hidden;
+}
+
+input, button {
+  appearance: none;
+  outline: none;
+  border: none;
+  background: none;
+}
+
+.button {
+  display: inline-block;
+  padding: 8px 16px;
+  background-color: var(--primary);
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-radius: 6px;
+  transition: 0.4s;
+}
+
+.button:hover {
+  background-color: var(--primary-alt);
+}
+
+.button.button-outline {
+  background-color: transparent;
+  border: 2px solid var(--primary);
+  padding: 6px 14px;
+  color: var(--primary);
+}
+
+.button.button-outline:hover {
+  color: #FFF;
+  background-color: var(--primary);
+}
+
+.button.button-small {
+  padding: 4px 8px;
+  font-size: 18px;
+  font-weight: 600;
+}
+
+.button.button-alert {
+  background-color: crimson;
+}
+
+.button-group {
+  display: flex;
+  margin: 0 -8px;
+}
+
+.button-group.group-end {
+  justify-content: flex-end;
+}
+
+.button-group .button {
+  margin: 0 8px;
+}
+</style>
---
+++ client/src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <h1>The System</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'TheHeader',
+    }
+</script>
+
+<style lang="scss" scoped>
+header {
+    display: block;
+    padding: 16px;
+    background-color: var(--dark);
+}
+
+header h1 {
+    color: #FFF;
+    font-size: 28px;
+}
+</style>
